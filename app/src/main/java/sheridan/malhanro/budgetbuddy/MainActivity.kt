fb57a6d921ec636d3be6f4828751a0bfdc5aefdb
jatinsharma <sharm212@sheridancollege.ca>
2022-03-11 12:57:27 -0500

    Fixed Layout and fixed an issue where app won't run

--- app/src/main/java/sheridan/malhanro/budgetbuddy/MainActivity.kt
+++ app/src/main/java/sheridan/malhanro/budgetbuddy/MainActivity.kt
@@ -19,6 +19,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
+
     }
 
 
