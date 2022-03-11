dbbfd254ec2237e78ac72809e8ca5863ff15af29
jatinsharma <sharm212@sheridancollege.ca>
2022-03-11 12:51:46 -0500

    Fixed error in manifest file

--- app/src/main/java/sheridan/malhanro/budgetbuddy/MainActivity.kt
+++ app/src/main/java/sheridan/malhanro/budgetbuddy/MainActivity.kt
@@ -13,43 +13,13 @@ import sheridan.malhanro.budgetbuddy.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        navController = navHostFragment.navController
-
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 
-   // override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-       // menuInflater.inflate(R.menu.menu_main, menu)
-   //     return true
-   // }
-
-  //  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-  //      return NavigationUI.onNavDestinationSelected(item, navController) ||
-  //              when (item.itemId) {
-
-  //                  else -> super.onOptionsItemSelected(item)
-   //             }
-   // }
 }
